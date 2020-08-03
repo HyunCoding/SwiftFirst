f461e43068a74732b9ef97f3e34255b0664da5e4
HyunCoding <aa01080809571@gmail.com>
2020-08-03 23:50:33 +0900

    Initial Commit

---
+++ SwiftFirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftFirst
+//
+//  Created by Hyun on 2020/08/03.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
